8ce46e9d4fe0fc735aca6d4420060566fd972398
Ronye <ronye.rocha@gmail.com>
2017-08-08 03:03:56 -0300

    controller e view index de fornecedor

---
+++ ControleEstoque.Web/Controllers/FornecedorController.cs
@@ -0,0 +1,19 @@
+﻿using ControleEstoque.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleEstoque.Web.Controllers
+{
+    public class FornecedorController : Controller
+    {
+        private StockEntities stock = new StockEntities();
+
+        public ActionResult Index()
+        {
+            return View(stock.Fornecedor.ToList());
+        }
+    }
+}
