fd26abea1de6614e3d1702e548d3be2b587b399a
Ronye <ronye.rocha@gmail.com>
2017-08-09 18:27:24 -0300

    modal de cadastro de novo fornecedor

--- ControleEstoque.Web/Controllers/FornecedorController.cs
+++ ControleEstoque.Web/Controllers/FornecedorController.cs
@@ -15,5 +15,54 @@ namespace ControleEstoque.Web.Controllers
         {
             return View(stock.Fornecedor.ToList());
         }
+
+        public ActionResult SalvarFornecedor(Fornecedor f)
+        {
+            string resultado = "OK";
+            List<string> mensagens = new List<string>();
+            string idSalvo = string.Empty;
+
+            if (!ModelState.IsValid)
+            {
+                resultado = "AVISO";
+                mensagens = ModelState.Values.SelectMany(m => m.Errors).Select(m => m.ErrorMessage).ToList();
+            }
+            else
+            {
+                try
+                {
+                    // Verifica se o fornecedor já existe
+                    var forn = stock.Fornecedor.FirstOrDefault(x => x.Id == f.Id);
+
+                    // Se não existe, adiciona e salva (create)
+                    if (forn == null)
+                    {
+                        forn = f;
+                        stock.Fornecedor.Add(forn);
+                        stock.SaveChanges();
+                    }
+                    // Se existe, salva somente as alterações (update)
+                    else
+                    {
+                        forn.Nome = f.Nome;
+                        forn.Telefone = f.Telefone;
+                        forn.Endereco = f.Endereco;
+                        forn.Cidade = f.Cidade;
+                        forn.Pais = f.Pais;
+                        stock.SaveChanges();
+                    }
+
+                    idSalvo = f.Id.ToString();
+                }
+                catch (Exception ex)
+                {
+                    resultado = "ERRO: " + ex.Message;
+                }
+
+
+            }
+
+            return Json(new { Resultado = resultado, Mensagens = mensagens, IdSalvo = idSalvo });
+        }
     }
 }
